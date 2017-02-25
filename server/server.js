daa2853470fb7d40bc2a109747e9ad3e26ff6ca4
nihitx <masnadhossain@live.com>
2017-02-25 13:48:32 +0200

    new adds

--- server/server.js
+++ server/server.js
@@ -5,22 +5,39 @@ const cors = require('cors');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var requestify = require('requestify');
-var request = require('request')
+var request = require('request');
+var helmet = require('helmet');
+var https = require('https');
+
+var app = express();
+app.use(helmet())
+// app.use(cors({
+//     origin: ['http://localhost:3000'],
+//     credentials: true
+// }));
+
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true
-}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(publicPath));
+
 
 // app.get('/getIOT', function (req, res , err) {
 //   request({
