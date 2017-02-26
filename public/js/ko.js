96187f3b70f148757e89445e57571a64fd9f1621
nihitx <masnadhossain@live.com>
2017-02-26 03:54:56 +0200

    small fix

--- public/js/ko.js
+++ public/js/ko.js
@@ -8,30 +8,6 @@ function AppViewModel() {
 
   self.information = function() {
     var x = {"amount" : self.amount()};
-    console.log(string);
-      $.ajax({
-          type: 'GET',
-          url: 'https://gentle-oasis-93873.herokuapp.com/getIOT',
-          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-          dataType: 'json',
-          data: x
-      })
-      .done(function(result) {
-        self.paymentPlan([]);
-        $.each(result, function (index, item) {
-                    self.paymentPlan.push(item);
-                });
-        console.log(self.paymentPlan());
-        self.firstMonth(self.paymentPlan()[0].total_payment_with_fee);
-      })
-      .fail(function(xhr, status, error) {
-          console.log(error);
-      })
-      .always(function(data){
-      });
-  }
-
-  self.signup = function() {
       $.ajax({
           type: 'GET',
           url: 'http://localhost:3000/getIOT',
