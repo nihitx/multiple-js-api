270261b714af03ca02575caf02514fff59c2df0c
nihitx <masnadhossain@live.com>
2017-02-26 03:43:29 +0200

    ko done, getting ready for react

--- public/js/ko.js
+++ public/js/ko.js
@@ -8,8 +8,30 @@ function AppViewModel() {
 
   self.information = function() {
     var x = {"amount" : self.amount()};
-    var string = JSON.stringify(x);
     console.log(string);
+      $.ajax({
+          type: 'GET',
+          url: 'http://localhost:3000/getIOT',
+          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          dataType: 'json',
+          data: x
+      })
+      .done(function(result) {
+        self.paymentPlan([]);
+        $.each(result, function (index, item) {
+                    self.paymentPlan.push(item);
+                });
+        console.log(self.paymentPlan());
+        self.firstMonth(self.paymentPlan()[0].total_payment_with_fee);
+      })
+      .fail(function(xhr, status, error) {
+          console.log(error);
+      })
+      .always(function(data){
+      });
+  }
+
+  self.signup = function() {
       $.ajax({
           type: 'GET',
           url: 'http://localhost:3000/getIOT',
